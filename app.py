bc7cd91b0deb5c6e2c41f49f7640b3d359df8956
LisandroLedesma <lichaledesma31@gmail.com>
2024-11-20 13:30:28 -0300

    act estilos, update posts y env

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 import os
 import markdown
+from dotenv import load_dotenv
+
 from drive_loader import DriveLoader
+
+load_dotenv()
 
 app = Flask(__name__)
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-CREDENTIALS_PATH = os.path.join(PROJECT_ROOT, 'config', 'service-account.json')
+CREDENTIALS_PATH = os.getenv('CREDENTIALS_PATH')
 drive_loader = DriveLoader(CREDENTIALS_PATH)
 POSTS_FOLDER = 'posts'
 os.makedirs(POSTS_FOLDER, exist_ok=True)
@@ -16,8 +19,16 @@ drive_loader.download_markdown_files(POSTS_FOLDER)
 
 def get_posts():
     """Obtiene todos los posts en formato Markdown"""
-    drive_loader.download_markdown_files(POSTS_FOLDER)
     posts = []
+    extensions = [
+        'tables',  # Tablas
+        'fenced_code',  # Bloques de código con acentos
+        'codehilite',  # Resaltado de sintaxis
+        'nl2br',  # Saltos de línea
+        'sane_lists',  # Listas más precisas
+        'smarty',  # Tipografía inteligente
+    ]
+
     for filename in os.listdir(POSTS_FOLDER):
         if filename.endswith('.md'):
             with open(os.path.join(POSTS_FOLDER, filename), 'r', encoding='utf-8') as f:
@@ -27,7 +38,7 @@ def get_posts():
                 posts.append({
                     'title': title,
                     'slug': filename.replace('.md', ''),
-                    'content': markdown.markdown(content)
+                    'content': markdown.markdown(content, extensions=extensions)
                 })
     return sorted(posts, key=lambda x: x['title'])
 
@@ -44,6 +55,11 @@ def post(slug):
     active_post = next((post for post in posts if post['slug'] == slug), None)
     return render_template('index.html', posts=posts, active_post=active_post)
 
+@app.route('/update_posts', methods=['POST'])
+def update_posts():
+    """Actualiza las entradas descargando los archivos Markdown del Drive"""
+    drive_loader.download_markdown_files(POSTS_FOLDER)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
